3707d6016cf29cd8159b4ea779e3fe2f49a20b89
alikeli <eli-com2@yandex.ru>
2023-03-08 17:05:32 +0100

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+//        if (dividend / divider * divider ==dividend ) {
+//            System.out.println("can be divided completely");
+//        }  else {
+//            System.out.println("cannot be divided completely");
+//        }
+//        if (divider <= 0) {
+//
+//                throw new IllegalArgumentException("division by zero");
+//            }
+
+
+        if (divider == 0.0) {
+            throw new IllegalArgumentException("division by zero");
+        } else if (dividend / divider * divider == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
     }
 }
+
+
+
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber > 0 && (monthNumber <= 2 || monthNumber == 12) ){
+            System.out.println("Winter");
+        } else if (monthNumber > 2 && monthNumber < 6 && monthNumber > 0 ) {
+            System.out.println("Spring");
+        } else if (monthNumber > 5 || monthNumber < 9 && monthNumber > 0) {
+            System.out.println("Summer");
+        } else if (monthNumber > 9 || monthNumber < 12 && monthNumber > 0 ) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
